430259eff8f8ace52d104955620fef61f34f376d
ychongjie <ychongjie@gmail.com>
2016-11-15 16:40:27 +0800

    项目初始化, 添加 Checkstyle, PMD 和 Findbugs 代码分析工具

---
+++ app/src/main/java/com/feicuiedu/eshop/EShopApplication.java
@@ -0,0 +1,21 @@
+package com.feicuiedu.eshop;
+
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+public class EShopApplication extends Application {
+
+    @Override public void onCreate() {
+        super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // 此进程是LeakCanary用于内存堆分析的.
+            // 不应该在此进程中做任何app初始化工作.
+            return;
+        }
+
+        LeakCanary.install(this);
+    }
+}
